d7c6c7d74ec92d5694ba86392c5af2aa0f05625f
Ignacio Galindo <joigama@gmail.com>
2012-04-13 17:46:32 -0500

    AdHoc connection with a CI server  * Retrieve data from a thoughtworks cruisecontrol.rb server  * Parse XML data and add ALL projects as menu items    * Show colored icon according to project status    * If at least one project fails, the panel button icon should be red as well.

--- src/extension.js
+++ src/extension.js
@@ -1,10 +1,11 @@
-const St = imports.gi.St;
-const Main = imports.ui.main;
+const St        = imports.gi.St;
+const Main      = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 
 
-let menuItem;
+const Soup = imports.gi.Soup;
+const CI_URL = 'http://cruisecontrolrb.thoughtworks.com/XmlStatusReport.aspx';
 
 function CIStatusButton() {
   this._init()
@@ -16,11 +17,38 @@ CIStatusButton.prototype = {
 
   _init: function() {
     PanelMenu.SystemStatusButton.prototype._init.call(this, 'cistatus-gray');
-
     this._iconActor.icon_type = St.IconType.FULLCOLOR;
 
-    menuItem = new PopupMenu.PopupMenuItem(_("Settings"));
-    this.menu.addMenuItem(menuItem);
+    this._getReport();
+  },
+
+  _getReport: function(){
+    let that = this
+    message = Soup.Message.new('GET', CI_URL);
+    _httpSession = new Soup.SessionAsync();
+    _httpSession.queue_message(message, function(){
+      that._updateStatus(new XML(message.response_body.data));
+    });
+  },
+
+  _updateStatus: function(data) {
+    failure = null;
+    for each(var project in data.Project) {
+      menu_item = new PopupMenu.PopupMenuItem(_(project.@name.toString()));
+      if (project.@lastBuildStatus == 'Success'){
+        icon_name = 'cistatus-green';
+      }
+      else{
+        icon_name = 'cistatus-red';
+        failure = true;
+      }
+
+      icon_object = new St.Icon({ icon_name: icon_name, icon_type: St.IconType.FULLCOLOR, icon_size: 16 });
+      menu_item.addActor(icon_object);
+
+      this.menu.addMenuItem(menu_item);
+    }
+    this._iconActor.icon_name = (failure == true) ? 'cistatus-red' : 'cistatus-green';
   },
 
   enable: function() {
