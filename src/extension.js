13147986f8e16cba8ed2366521bf31a1cdeee09c
Ignacio Galindo <joigama@gmail.com>
2012-04-16 15:18:05 -0500

    Just a big commit - Follow some advices http://goo.gl/KKCYe - Refactor favoring readability and styling - Added a main loop in order to get periodic updates

--- src/extension.js
+++ src/extension.js
@@ -2,10 +2,19 @@ const St        = imports.gi.St;
 const Main      = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
+const Soup      = imports.gi.Soup;
+const Mainloop  = imports.mainloop;
+const Lang      = imports.lang;
+
+// Prevent Session from being garbage collected http://goo.gl/KKCYe
+const Session   = new Soup.SessionAsync();
+
+// Allow Session work under a proxy http://goo.gl/KKCYe
+Soup.Session.prototype.add_feature.call(Session, new Soup.ProxyResolverDefault());
 
 
-const Soup = imports.gi.Soup;
 const CI_URL = 'http://cruisecontrolrb.thoughtworks.com/XmlStatusReport.aspx';
+const LOOP_INTERVAL = 60;
 
 function CIStatusButton() {
   this._init()
@@ -19,44 +28,79 @@ CIStatusButton.prototype = {
     PanelMenu.SystemStatusButton.prototype._init.call(this, 'cistatus-gray');
     this._iconActor.icon_type = St.IconType.FULLCOLOR;
 
-    this._getReport();
   },
 
-  _getReport: function(){
-    let that = this
+  _getCruiseControlReport: function() {
+    if (typeof(self) != 'undefined') {
+      self.menu.removeAll();
+    }
+
+    self = this;
     message = Soup.Message.new('GET', CI_URL);
-    _httpSession = new Soup.SessionAsync();
-    _httpSession.queue_message(message, function(){
-      that._updateStatus(new XML(message.response_body.data));
+
+    Session.queue_message(message, function() {
+      if ((data = message.response_body.data) != null) {
+        self._updateStatus(new XML(data));
+      }
     });
+
+
+    Mainloop.timeout_add_seconds(LOOP_INTERVAL, Lang.bind(this, this._getCruiseControlReport));
+  },
+
+  _newStatusIcon: function(iconName) {
+    icon = new St.Icon({
+      icon_name: iconName,
+      icon_type: St.IconType.FULLCOLOR,
+      icon_size: 16
+    });
+    return icon
+  },
+
+  _newMenuItem: function(itemName){
+    item = new PopupMenu.PopupMenuItem(_(itemName));
+    return item
   },
 
   _updateStatus: function(data) {
-    failure = null;
+
     for each(var project in data.Project) {
-      menu_item = new PopupMenu.PopupMenuItem(_(project.@name.toString()));
-      if (project.@lastBuildStatus == 'Success'){
-        icon_name = 'cistatus-green';
-      }
-      else{
-        icon_name = 'cistatus-red';
-        failure = true;
+      projectName   = project.@name.toString();
+      projectStatus = project.@lastBuildStatus.toString();
+
+      switch(projectStatus) {
+        case 'Success':
+          iconName = 'cistatus-green';
+          break;
+        case 'Failure':
+          iconName = 'cistatus-red';
+          anyFailure = true;
+          break;
+        default:
+          iconName = 'cistatus-gray';
       }
 
-      icon_object = new St.Icon({ icon_name: icon_name, icon_type: St.IconType.FULLCOLOR, icon_size: 16 });
-      menu_item.addActor(icon_object);
+      menuItem = this._newMenuItem(projectName);
+      menuItem.addActor(this._newStatusIcon(iconName));
 
-      this.menu.addMenuItem(menu_item);
+      this.menu.addMenuItem(menuItem);
     }
-    this._iconActor.icon_name = (failure == true) ? 'cistatus-red' : 'cistatus-green';
+
+    globalStatus = anyFailure == true ? 'cistatus-red' : 'cistatus-green';
+    this._iconActor.icon_name = globalStatus;
   },
 
   enable: function() {
     Main.panel._rightBox.insert_actor(this.actor, 0);
     Main.panel._menus.addMenu(this.menu);
+
+    this._mainloop = Mainloop.timeout_add(0, Lang.bind(this, function() {
+      this._getCruiseControlReport();
+    }));
   },
 
   disable: function() {
+    Mainloop.source_remove(this._mainloop);
     Main.panel._menus.removeMenu(this.menu);
     Main.panel._rightBox.remove_actor(this.actor);
   }
