b14243e06f74fa276b0a7660d5c28fee2385b466
Ignacio Galindo <joigama@gmail.com>
2012-06-01 03:54:51 -0500

    Super duper refactor, to add privacy to indicator module and format its code

--- src/extension.js
+++ src/extension.js
@@ -26,249 +26,242 @@ Soup.Session.prototype.add_feature.call(Session,
                                         new Soup.ProxyResolverDefault());
 
 function Indicator() {
-    this._init()
+    return this._init()
 }
 
 Indicator.prototype = {
 
-  __proto__: PanelMenu.ButtonBox.prototype,
+    __proto__: PanelMenu.ButtonBox.prototype,
 
-  _init: function(metadata) {
-      PanelMenu.ButtonBox.prototype._init.call(this, { reactive: true });
+    _init: function(metadata) {
+        PanelMenu.ButtonBox.prototype._init.call(this, { reactive: true });
 
-      this._source = new MsgTray.SystemNotificationSource();
+        this._source = new MsgTray.SystemNotificationSource();
 
-      this._settings = new Settings.Editor(this._source);
+        this._settings = new Settings.Editor(this._source);
 
-      this._buildControls();
+        this._buildControls();
 
-      Main.messageTray.add(this._source); // Should I move this somewhere else ?
-  },
+        Main.messageTray.add(this._source); // Should I move this ?
 
-  // Build indicator controls
-  _buildControls: function() {
-    this.actor.add_actor(Icons.get('settings-gear.png'));
+        var self =  this;
 
-    this._leftMenu = new PopupMenu.PopupMenu(this.actor, 0.0, St.Side.TOP);
-    this._leftMenu.actor.hide();
+        return {
+            enable: function() {
+                Main.panel._rightBox.insert_child_at_index(self.actor, 0);
+                Main.uiGroup.add_actor(self._rightMenu.actor);
+                Main.panel._menus.addMenu(self._rightMenu);
+                Main.uiGroup.add_actor(self._leftMenu.actor);
+                Main.panel._menus.addMenu(self._leftMenu);
 
-    this._unnableToConnectLabel = new St.Label({
-      text: 'Unable to connect..',
-      style_class: 'cistatus-menu-label'
-    });
+                self._connectControls();
+                self._settings.enable();
+                ProjectsDialog.enable();
 
-    this._leftMenu.addActor(this._unnableToConnectLabel);
+                if (self._settings.read()) {
+                    let mainloopCallback = Lang.bind(self, function() {
+                        self._getStatusReport();
+                    });
+                    self._mainloop = Mainloop.timeout_add(0, mainloopCallback);
+                }
+            },
 
-    this._projectsMenuItems = [];
+            disable: function() {
+                Mainloop.source_remove(self._mainloop);
 
-    this._rightMenu = new PopupMenu.PopupMenu(this.actor, 0.0, St.Side.TOP);
-    this._rightMenu.actor.hide();
+                ProjectsDialog.disable();
+                self._settings.disable();
+                self._disconnectControls();
+                self._removeProjectsMenuItems();
 
-    this._projectsMenuItem = this._newMenuItem("Projects");
-    this._projectsMenuItem.addActor(Icons.get('projects.png'));
-    this._rightMenu.addMenuItem(this._projectsMenuItem);
+                Main.uiGroup.remove_actor(self._leftMenu.actor);
+                Main.panel._menus.removeMenu(self._leftMenu);
+                Main.uiGroup.remove_actor(self._rightMenu.actor);
+                Main.panel._menus.removeMenu(self._rightMenu);
+                Main.panel._rightBox.remove_actor(self.actor);
+            }
+        };
+    },
 
-    this._settingsMenuItem = this._newMenuItem("Settings");
-    this._settingsMenuItem.addActor(Icons.get('settings-gear.png'));
-    this._rightMenu.addMenuItem(this._settingsMenuItem);
-  },
+    // Build indicator controls
+    _buildControls: function() {
+        this.actor.add_actor(Icons.get('settings-gear.png'));
 
-  // Connect signal handlers
-  _connectControls: function() {
-    this._indicatorOnClickId = this.actor.connect(
-      'button-press-event', Lang.bind(this, this._onClick)
-    );
+        this._leftMenu = new PopupMenu.PopupMenu(this.actor, 0.0, St.Side.TOP);
+        this._leftMenu.actor.hide();
 
-    this._onGlobalStatusChangeId = this.connect(
-      'global-status-change', Lang.bind(this, this._onGlobalStatusChange)
-    );
+        this._responseError = new St.Label({ style_class: 'cistatus-menu-label',
+                                             text: 'Unable to connect' });
 
-    this._onProjectsRefreshId = this.connect(
-      'projects-refresh', Lang.bind(this, this._onProjectsRefresh)
-    );
+        this._leftMenu.addActor(this._responseError);
 
-    this._projectsMenuItemOnClickId = this._projectsMenuItem.actor.connect(
-      'button-press-event', Lang.bind(ProjectsDialog, ProjectsDialog.open)
-    );
+        this._projectsMenuItems = [];
 
-    this._settingsMenuItemOnClickId =  this._settingsMenuItem.actor.connect(
-      'button-press-event', Lang.bind(this._settings, this._settings.open)
-    );
+        this._rightMenu = new PopupMenu.PopupMenu(this.actor, 0.0, St.Side.TOP);
+        this._rightMenu.actor.hide();
 
-  },
+        this._projectsMenuItem = new PopupMenu.PopupMenuItem(_("Projects"));
+        this._projectsMenuItem.addActor(Icons.get('projects.png'));
+        this._rightMenu.addMenuItem(this._projectsMenuItem);
 
-  // Disconnect signal handlers
-  _disconnectControls: function() {
-    this.actor.disconnect(this._indicatorOnClickId);
-    this.disconnect(this._onGlobalStatusChangeId);
-    this.disconnect(this._onProjectsRefreshId);
-    this._projectsMenuItem.actor.disconnect(this._projectsMenuItemOnClickId);
-    this._settingsMenuItem.actor.disconnect(this._settingsMenuItemOnClickId);
-  },
+        this._settingsMenuItem = new PopupMenu.PopupMenuItem(_("Settings"));
+        this._settingsMenuItem.addActor(Icons.get('settings-gear.png'));
+        this._rightMenu.addMenuItem(this._settingsMenuItem);
+    },
 
-  // Get CI's report
-  _getStatusReport: function() {
-    this.emit('projects-refresh');
+    // Connect signal handlers
+    _connectControls: function() {
+        this._onClickId = this.actor.connect('button-press-event',
+                                             Lang.bind(this, this._onClick));
 
-    let message = Soup.Message.new('GET', this._settings.preferences.url);
+        this._onStatusChangeId = this.connect('status-change',
+                                              Lang.bind(this,
+                                                        this._onStatusChange));
 
-    let self = this;
+        this._onSyncId = this.connect('status-sync',
+                                      Lang.bind(this, this._onSync));
 
-    Session.queue_message(message, function() {
-      let data = message.response_body.data;
-      if (data != null) {
-        self._updateStatus(new XML(data));
-      }
-      else {
-        self._globalStatus = 'status-unknown';
-        self.emit('global-status-change');
-      }
-    });
+        this._onProjectsClickId = this._projectsMenuItem.actor.connect(
+          'button-press-event',
+          Lang.bind(ProjectsDialog, ProjectsDialog.open));
 
-    Mainloop.timeout_add_seconds(
-      this._settings.preferences.interval,
-      Lang.bind(this, this._getStatusReport)
-    );
-  },
+        this._onSettingsClickId =  this._settingsMenuItem.actor.connect(
+          'button-press-event',
+          Lang.bind(this._settings, this._settings.open));
 
-  // Build new project menu item
-  _newMenuItem: function(itemName){
-    return new PopupMenu.PopupMenuItem(_(itemName));
-  },
+    },
 
-  // Dual click logic
-  _onClick: function(actor, event) {
-    switch(event.get_button()){
-      case 1 :
-        this._rightMenu.isOpen ? this._rightMenu.close() : undefined;
-        this._leftMenu.toggle();
-        break;
-      case 3 :
-        this._leftMenu.isOpen ? this._leftMenu.close(): undefined;
-        this._rightMenu.toggle();
-        break;
-      default:
-        break;
-    }
-  },
+    // Disconnect signal handlers
+    _disconnectControls: function() {
+        this.actor.disconnect(this._onClickId);
+        this.disconnect(this._onStatusChangeId);
+        this.disconnect(this._onSyncId);
+        this._projectsMenuItem.actor.disconnect(this._onProjectsClickId);
+        this._settingsMenuItem.actor.disconnect(this._onSettingsClickId);
+    },
 
-  // On global status changed callback
-  _onGlobalStatusChange: function() {
-    this.actor.destroy_all_children();
-    this.actor.add_actor(Icons.get(this._globalStatus + '.png'));
+    // Get CI's report
+    _getStatusReport: function() {
+        this.emit('status-sync');
 
-    if (this._globalStatus == 'status-unknown'){
-      this._removeProjectsMenuItems();
-      this._unnableToConnectLabel.show();
-    }
-    else {
-      this._unnableToConnectLabel.hide();
-    }
-  },
+        let message = Soup.Message.new('GET', this._settings.preferences.url);
 
-  // Handle projects refresh: Change indicator icon
-  _onProjectsRefresh: function() {
-    this._globalStatus = 'status-refresh';
-    this.emit('global-status-change');
-  },
+        let self = this;
 
-  // Disconnect event signals and remove projects menu items
-  _removeProjectsMenuItems: function() {
-     if(this._projectsMenuItems.length > 0) {
-      for each(let projectMenuItem in this._projectsMenuItems) {
-        projectMenuItem.menu_item.actor.disconnect(projectMenuItem.event_id);
-        projectMenuItem.menu_item.actor.destroy_all_children();
-      };
-      this._projectsMenuItems = [];
-      this._leftMenu.removeAll();
-    };
-  },
+        Session.queue_message(message, function() {
+            let data = message.response_body.data;
+            if (data != null) {
+                self._updateStatus(new XML(data));
+            }
+            else {
+                self._globalStatus = 'status-unknown';
+                self.emit('status-change');
+            }
+        });
 
-  // Update project menu items and their statuses
-  _updateStatus: function(data) {
-    let anyFailure;
+        Mainloop.timeout_add_seconds(this._settings.preferences.interval,
+                                     Lang.bind(this, this._getStatusReport));
+    },
 
-    this._removeProjectsMenuItems();
+    // Dual click logic
+    _onClick: function(actor, event) {
+        if (event.get_button() == 1) {
+            if (this._rightMenu.isOpen)
+                this._rightMenu.close();
+            this._leftMenu.toggle();
+        }
+        else {
+            if (this._leftMenu.isOpen)
+                this._leftMenu.close();
+            this._rightMenu.toggle();
+        }
+    },
 
-    for each(let project in data.Project) {
+    // On global status changed callback
+    _onStatusChange: function() {
+        this.actor.destroy_all_children();
+        this.actor.add_actor(Icons.get(this._globalStatus + '.png'));
 
-      let projectName   = project.@name.toString();
-      let projectStatus = project.@lastBuildStatus.toString();
-      let projectUrl    = project.@webUrl.toString();
+        if (this._globalStatus == 'status-unknown'){
+            this._removeProjectsMenuItems();
+            this._responseError.show();
+        }
+        else {
+            this._responseError.hide();
+        }
+    },
 
-      let iconName;
+    // Handle projects refresh: Change indicator icon
+    _onSync: function() {
+        this._globalStatus = 'status-sync';
+        this.emit('status-change');
+    },
 
-      switch(projectStatus) {
-        case 'Success':
-          iconName = 'status-pass';
-          break;
-        case 'Failure':
-          iconName = 'status-fail';
-          anyFailure = true;
-          break;
-        default:
-          iconName = 'status-unknown';
-      }
+    // Disconnect event signals and remove projects menu items
+    _removeProjectsMenuItems: function() {
+         if(this._projectsMenuItems.length > 0) {
+             for each(let project in this._projectsMenuItems) {
+                 project.menu_item.actor.disconnect(project.event_id);
+                 project.menu_item.actor.destroy_all_children();
+              };
+              this._projectsMenuItems = [];
+              this._leftMenu.removeAll();
+        };
+    },
 
-      let menuItem = this._newMenuItem(projectName);
-      menuItem.addActor(Icons.get(iconName + '.png'));
+    // Update project menu items and their statuses
+    _updateStatus: function(data) {
+        let anyFailure;
 
-      let menuItemOnClickId = menuItem.actor.connect(
-        'button-press-event', Lang.bind(this, function() {
-          this._visitProjectUrl(projectUrl);
-        })
-      );
+        this._removeProjectsMenuItems();
 
-      this._projectsMenuItems.push({
-        menu_item: menuItem,
-        event_id: menuItemOnClickId
-      });
+        for each(let project in data.Project) {
 
-      this._leftMenu.addMenuItem(menuItem);
-    }
+            let projectName   = project.@name.toString();
+            let projectStatus = project.@lastBuildStatus.toString();
+            let projectUrl    = project.@webUrl.toString();
 
-    this._globalStatus = anyFailure == true ? 'status-fail' : 'status-pass';
-    this.emit('global-status-change');
-  },
+            let iconName;
 
-  // Open project url in the default browser
-  _visitProjectUrl: function(projectUrl) {
-    GLib.spawn_async_with_pipes(null, ["gnome-www-browser", "-m", projectUrl], null,
-                                GLib.SpawnFlags.SEARCH_PATH, null);
-  },
+            switch(projectStatus) {
+                case 'Success':
+                    iconName = 'status-pass';
+                    break;
+                case 'Failure':
+                    iconName = 'status-fail';
+                    anyFailure = true;
+                    break;
+                default:
+                    iconName = 'status-unknown';
+            }
 
-  enable: function() {
-    Main.panel._rightBox.insert_child_at_index(this.actor, 0);
-    Main.uiGroup.add_actor(this._rightMenu.actor);
-    Main.panel._menus.addMenu(this._rightMenu);
-    Main.uiGroup.add_actor(this._leftMenu.actor);
-    Main.panel._menus.addMenu(this._leftMenu);
+            let menuItem = new PopupMenu.PopupMenuItem(_(projectName));
+            menuItem.addActor(Icons.get(iconName + '.png'));
 
-    this._connectControls();
-    this._settings.enable();
-    ProjectsDialog.enable();
+            let onMenuItemCallback = Lang.bind(this, function() {
+                this._visitProjectUrl(projectUrl);
+            });
 
-    if (this._settings.read()) {
-      this._mainloop = Mainloop.timeout_add(0, Lang.bind(this, function() {
-        this._getStatusReport();
-      }));
-    }
-  },
+            let onMenuItemClickId = menuItem.actor.connect('button-press-event',
+                                                           onMenuItemCallback);
 
-  disable: function() {
-    Mainloop.source_remove(this._mainloop);
+            this._projectsMenuItems.push({ menu_item: menuItem,
+                                           event_id: onMenuItemClickId });
 
-    ProjectsDialog.disable();
-    this._settings.disable();
-    this._disconnectControls();
-    this._removeProjectsMenuItems();
+            this._leftMenu.addMenuItem(menuItem);
+        }
 
-    Main.uiGroup.remove_actor(this._leftMenu.actor);
-    Main.panel._menus.removeMenu(this._leftMenu);
-    Main.uiGroup.remove_actor(this._rightMenu.actor);
-    Main.panel._menus.removeMenu(this._rightMenu);
-    Main.panel._rightBox.remove_actor(this.actor);
-  }
+        this._globalStatus = anyFailure == true ? 'status-fail' : 'status-pass';
+        this.emit('status-change');
+    },
+
+    // Open project url in the default browser
+    _visitProjectUrl: function(url) {
+        GLib.spawn_async_with_pipes(null, ["gnome-www-browser", "-m", url],
+                                    null, GLib.SpawnFlags.SEARCH_PATH, null);
+    },
+
+
 }
 
 Signals.addSignalMethods(Indicator.prototype);
